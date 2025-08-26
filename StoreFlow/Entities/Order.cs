a9ad59e4932e2628f5a316aef0b1acc85f4d98b5
Developer <Developer@DEV>
2025-08-26 16:53:26 +0300

    Add project files.

---
+++ StoreFlow/Entities/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace StoreFlow.Entities
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int CustomerId { get; set; }
+        public int OrderCount { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime SaleDate { get; set; }
+    }
+}
